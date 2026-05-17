2046126959a715d6da747974bad01b01ccfb2c38
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-05-17 18:28:11 -0300

    up

--- Database/obj/Debug/net8.0/Database.AssemblyInfo.cs
+++ Database/obj/Debug/net8.0/Database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b68033441618029c5b0881e15d73fbb7d12039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0d331fe8d6b986625059c23491055109d63bad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
